bd63cc12fa1133ab467b8917a7faeb0a1cd27f35
SerSantin <sergio.ms.91@gmail.com>
2016-01-08 16:45:28 +0000

    1st Commit.  - Realizadas todas las funcionalidades (Probadas y Corriendo).  - Esperando Informe.

---
+++ src/Interfaces/iGraficos.java
@@ -0,0 +1,19 @@
+package Interfaces;
+
+import PRC7.Dash;
+import PRC7.Dataset;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sergio
+ */
+public interface iGraficos {
+    public void pintar(javax.swing.JPanel p, int pos);
+    
+}
